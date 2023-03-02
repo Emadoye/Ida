7f622e0921ce78ef79e5b915e4273b9c39b07ddf
Emadoye <joanneemadoye2@gmail.com>
2023-03-02 08:51:56 +0100

    To set the output of sub2

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-window.setInterval(isPlaying,5000) //HTML to set timer
+window.setInterval(second1,5000) //HTML to set timer
 let myVideo = document.getElementById("video1")
 let myButton = document.getElementById("button1")
+let myDataTrackStr = myVideo.getAttribute('data-track')
+let dataTrackObj = JSON.parse(myDataTrackStr)
+
+
 function changeVideo() {
     //Let the button report if video is playing or not
     if (myVideo.paused) {
@@ -16,11 +20,11 @@ function changeVideo() {
 function isPlaying() {
     //To report if video is playing or not
     if (myVideo.paused) {
-        document.getElementById("heading1").innerHTML = "Video is not playing"
+        
         return false
     }
     else {
-        document.getElementById("heading1").innerHTML = "Video is playing"
+        
         return true
     }
 }
@@ -29,9 +33,11 @@ function second1() {
     let playing = isPlaying() 
     if (playing) {
         //return value of sub2 
+        document.getElementById("heading1").innerHTML = dataTrackObj.param2.sub2
+        console.log(dataTrackObj.param2.sub2)
 
     }
     else {
-        return "Warning"
+        document.getElementById("heading1").innerHTML =  "Warning"
     }
 }
